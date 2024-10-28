61f6edf36bf47386935d8746de8a17e2b1d22c84
Joshua McD <77360758+ExpensiveCoder@users.noreply.github.com>
2024-10-28 16:31:55 -0500

    Create abilitydb.py
    Holds a python dictionary of every ability in cfb25 with tiers and attribute thresholds.

---
+++ abilitydb.py
@@ -0,0 +1,21 @@
+
+
+Ability = {
+  "360": {
+    "Attribute": "Spin Move",
+    "Bronze": {
+      "Rating": 80,
+      "Description": "Slightly improved ability to fake out defenders with Spin moves"
+    },
+    "Silver": {
+      "Rating": 85,
+      "Description": "Moderately improved ability to fake out defenders with Spin moves"
+    }
+  },
+  "50/50": {
+    "Attribute": "Spec Catch",
+    "Bronze": {
+      "Rating": 80,
+      "Description": "With positional advantage, slightly improved 1v1 jump balls 30+ yards downfield or in the end zone."
+    }
+  },
