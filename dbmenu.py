09ba71e95d10198f66f4d8c150bde716a7d28913
Joshua McD <77360758+ExpensiveCoder@users.noreply.github.com>
2024-10-30 17:45:20 -0500

    Update dbmenu.py

--- dbmenu.py
+++ dbmenu.py
@@ -1,23 +1,37 @@
 import os
 from cfb_db import *
 
+# Main Menu for Program Startup
 def menu():
     os.system('cls')
     print("CFB Player Database")
     print("-----------------------")
     print("1. View Players")
-    print("2. Pitch Calculator")
-    print("3. Ability Database")
-    print("4. Export Data")
+    print("2. Update Player (Stats)")
+    print("3. Pitch Calculator (N/I)")
+    print("4. Ability Database (N/I)")
     print("5. Exit")
     
     choice = input("Enter your choice: ")
     return choice
     
+# Menu for View Player
 def playerMenu():
     print("-"*70)
-    print("1. Add Player  |  2. Update Player")
-    print("3. Cut Player  |  4. Exit")
+    print("1. Add Player   |   2. Cut Player")
+    print("3. Change VIew  |   4. Exit")
     
     choice = input("Enter your choice: ")
+    return choice
+
+# TODO: Menu for Updating players
+def updateMenu():
+    
+    choice = input("Enter a Menu Option: ")
+    return choice
+
+# TODO: Menu for different Views
+def viewMenu():
+    
+    choice = input("Enter a Menu Option: ")
     return choice
