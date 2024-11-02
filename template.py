d03fdc469a55446a87b80ddf94351392bfc3619a
Joshua McD <77360758+ExpensiveCoder@users.noreply.github.com>
2024-11-02 14:45:10 -0500

    Update template.py

--- template.py
+++ template.py
@@ -1,2 +1,45 @@
-# TODO: Ability Dictionary
-# TODO: Archetype Dictionary
+# Ability Dictionary
+Ability_Template = { 
+    "Ability Name": {
+    "Type": "",
+    "Attribute": "",
+    "ShortDes": "",
+    "Archetypes": "",
+    "Bronze": {
+      "Rating": 0,
+      "Description": ""
+    },
+    "Silver": {
+      "Rating": 0,
+      "Description": ""
+    },
+    "Gold": {
+      "Rating": 0,
+      "Description": ""
+    },
+    "Platinum": {
+      "Rating": 0,
+      "Description": ""
+    }
+  }
+}
+
+# Archetype Dictionary
+Archetype_Template = {
+  "Archetype Name": {
+    "Position": {
+      "Ability1": "Ability Name",
+      "Ability2": "Ability Name",
+      "Ability3": "Ability Name",
+      "Ability4": "Ability Name",
+      "Ability5": "Ability Name"
+    },
+    "Position2": {
+      "Ability1": "Ability Name",
+      "Ability2": "Ability Name",
+      "Ability3": "Ability Name",
+      "Ability4": "Ability Name",
+      "Ability5": "Ability Name"
+    }
+  }
+}
