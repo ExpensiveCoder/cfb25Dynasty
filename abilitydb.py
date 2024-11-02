08063094a31cb0db95d338efcaeaa7709a7b36f2
Joshua McD <77360758+ExpensiveCoder@users.noreply.github.com>
2024-11-02 08:53:06 -0500

    Update abilitydb.py

--- abilitydb.py
+++ abilitydb.py
@@ -1,9 +1,33 @@
-
+Template = { 
+    "Ability Name": {
+    "Type": "",
+    "Attribute": "",
+    "ShortDes": "",
+    "Archetypes": "",
+    "Bronze": {
+      "Rating": 0,
+      "Description": ""
+    },
+    "Silver": {
+      "Rating": 0,
+      "Description": ""
+    },
+    "Gold": {
+      "Rating": 0,
+      "Description": ""
+    },
+    "Platinum": {
+      "Rating": 0,
+      "Description": ""
+    }
+  }
+}
 
 Ability = {
   "360": {
     "Type": "Physical",
     "Attribute": "Spin Move",
+    "ShortDes": ""
     "Archetypes": "Elusive Back, ",
     "Bronze": {
       "Rating": 80,
@@ -12,12 +36,105 @@ Ability = {
     "Silver": {
       "Rating": 85,
       "Description": "Moderately improved ability to fake out defenders with Spin moves"
+    },
+    "Gold": {
+      "Rating": 0,
+      "Description": "Significantly improved ability to fake out defenders with Spin moves and increased responsiveness."
+    },
+    "Platinum": {
+      "Rating": 0,
+      "Description": "Ultimate spin moves for responsiveness and faking out defenders."
     }
   },
   "50/50": {
+    "Type": "Physical",
     "Attribute": "Spec Catch",
+    "ShortDes": "Improved jump ball ability"
+    "Archetypes": "Deep Threat, ",
     "Bronze": {
-      "Rating": 80,
+      "Rating": 89,
       "Description": "With positional advantage, slightly improved 1v1 jump balls 30+ yards downfield or in the end zone."
+    },
+    "Silver": {
+      "Rating": 0,
+      "Description": "With positional advantage, moderately improved 1v1 jump balls 30+ yards downfield or in the end zone."
+    },
+    "Gold": {
+      "Rating": 0,
+      "Description": "With positional advantage, significantly improved 1v1 jump balls 20+ yards downfield or in the end zone."
+    },
+    "Platinum": {
+      "Rating": 0,
+      "Description": "With positional advantage, ultimate ability to win 1v1 jump balls 15+ yards downfield or in the end zone."
     }
   },
+  "Aftershock": {
+    "Type": "Physical",
+    "Attribute": "",
+    "ShortDes": "",
+    "Archetypes": "",
+    "Bronze": {},
+    "Silver": {},
+    "Gold": {},
+    "Platinum": {}
+  },
+  "Arm Bar": {
+    "Type": "Physical",
+    "Attribute": "",
+    "ShortDes": "",
+    "Archetypes": "",
+    "Bronze": {},
+    "Silver": {},
+    "Gold": {},
+    "Platinum": {}
+  },
+  "Balanced": {
+    "Type": "Physical",
+    "Attribute": "",
+    "ShortDes": ""
+    "Archetypes": "Deep Threat, ",
+    "Bronze": {
+      "Rating": 0,
+      "Description": ""
+    },
+    "Silver": {
+      "Rating": 0,
+      "Description": ""
+    },
+    "Gold": {},
+    "Platinum": {}
+  },
+  
+  
+  "Press Pro": {
+    "Type": "Physical"
+    "Attribute": "",
+    "ShortDes": ""
+    "Archetypes": "Deep Threat, ",
+    "Bronze": {},
+    "Silver": {},
+    "Gold": {},
+    "Platinum": {}
+  },
+  "Recoup": {
+    "Type": "Physical"
+    "Attribute": "",
+    "ShortDes": ""
+    "Archetypes": "Deep Threat, ",
+    "Bronze": {},
+    "Silver": {},
+    "Gold": {},
+    "Platinum": {}
+  },
+  "Sure Hands": {
+    "Type": "Physical"
+    "Attribute": "",
+    "ShortDes": ""
+    "Archetypes": "Deep Threat, ",
+    "Bronze": {},
+    "Silver": {},
+    "Gold": {},
+    "Platinum": {}
+  },
+  
+}
