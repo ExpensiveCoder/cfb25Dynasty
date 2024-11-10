7d938590297e4b72ff3b583fa67206d854196cd6
Joshua McD <77360758+ExpensiveCoder@users.noreply.github.com>
2024-11-10 17:52:46 -0600

    Update abilitydb.py

--- abilitydb.py
+++ abilitydb.py
@@ -73,10 +73,19 @@ Ability = {
     "Attribute": "",
     "ShortDes": "",
     "Archetypes": "",
-    "Bronze": {},
-    "Silver": {},
-    "Gold": {},
-    "Platinum": {}
+    "Bronze": {
+      "Rating": 0,
+      "Description": "Slightly increased wear and tear effects to opposing players on tackles."},
+    "Silver": {
+      "Rating": 0,
+      "Description": "Moderately increased wear and tear effects to opposing players on tackles."},
+    "Gold": {
+      "Rating": 0,
+      "Description": "Significantly increased wear and tear effects to opposing players on tackles."},
+    "Platinum": {
+      "Rating": 0,
+      "Description": "Ultimate wear and tear effects to opposing players on tackles."
+    }
   },
   "Arm Bar": {
     "Type": "Physical",
@@ -104,7 +113,221 @@ Ability = {
     "Gold": {},
     "Platinum": {}
   },
-  
+  "Ballhawk": {
+    "Type": "Physical",
+    "Attribute": "",
+    "ShortDes": "",
+    "Archetypes": "",
+    "Bronze": {},
+    "Silver": {},
+    "Gold": {},
+    "Platinum": {}
+  },
+  "Best Friend": {
+    "Type": "Mental",
+    "ShortDes": "",
+    "Positions": "WR, TE",
+    "Bronze": {
+      "Description": "Slight composure gain for the QB when making a first down catch on third or fourth down."
+    },
+    "Silver": {
+      "Description": "Moderate composure gain for the QB when making a first down catch on third or fourth down."
+    },
+    "Gold": {
+      "Description": "Significant composure gain for the QB when making a first down catch on third or fourth down."
+    },
+    "Platinum": {
+      "Description": "Ultimate composure gain for the QB when making a first down catch on third or fourth down."
+    }
+  },
+  "Blanket Coverage": {
+    "Type": "Physical",
+    "Attribute": "",
+    "ShortDes": "",
+    "Archetypes": "",
+    "Bronze": {},
+    "Silver": {},
+    "Gold": {},
+    "Platinum": {}
+  },
+  "Blow Up": {
+    "Type": "Physical",
+    "Attribute": "",
+    "ShortDes": "",
+    "Archetypes": "",
+    "Bronze": {},
+    "Silver": {},
+    "Gold": {},
+    "Platinum": {}
+  },
+  "Bouncer": {
+    "Type": "Physical",
+    "Attribute": "",
+    "ShortDes": "",
+    "Archetypes": "",
+    "Bronze": {},
+    "Silver": {},
+    "Gold": {},
+    "Platinum": {}
+  },
+  "Chip Shot": {
+    "Type": "Physical",
+    "Attribute": "",
+    "ShortDes": "",
+    "Archetypes": "",
+    "Bronze": {},
+    "Silver": {},
+    "Gold": {},
+    "Platinum": {}
+  },
+  "Clearheaded": {
+    "Type": "Mental",
+    "ShortDes": "",
+    "Positions": "",
+    "Bronze": {
+      "Description": "Slight protection from composure loss."
+    },
+    "Silver": {
+      "Description": "Moderate protection from composure loss."
+    },
+    "Gold": {
+      "Description": "Significant protection from composure loss."
+    },
+    "Platinum": {
+      "Description": "Ulitmate protection from composure loss."
+    }
+  },
+  "Clutch Kicker": {
+    "Type": "Mental",
+    "ShortDes": "",
+    "Positions":, "",
+    "Bronze": {
+      "Description": "Slightly easier accuracy timing on field goals with two or less minutes remaining."
+    },
+    "Silver": {
+      "Description": "Moderately easier accuracy timing on field goals with two or less minutes remaining."
+    },
+    "Gold": {
+      "Description": "Significantly easier accuracy timing on field goals with two or less minutes remaining."
+    },
+    "Platinum": {
+      "Description": "Ultimate accuracy timing on field goals with two or less minutes remaining."
+    }
+  },
+  "Coffin Corner": {
+    "Type": "Physical",
+    "Attribute": "",
+    "ShortDes": "",
+    "Archetypes": "",
+    "Bronze": {},
+    "Silver": {},
+    "Gold": {},
+    "Platinum": {}
+  },
+  "Cutter": {
+    "Type": "Physical",
+    "Attribute": "",
+    "ShortDes": "",
+    "Archetypes": "",
+    "Bronze": {},
+    "Silver": {},
+    "Gold": {},
+    "Platinum": {}
+  },
+  "Deep Range": {
+    "Type": "Physical",
+    "Attribute": "",
+    "ShortDes": "",
+    "Archetypes": "",
+    "Bronze": {},
+    "Silver": {},
+    "Gold": {},
+    "Platinum": {}
+  },
+  "Defensive Rally": {
+    "Type": "Mental",
+    "ShortDes": "",
+    "Positions": "",
+    "Bronze": {
+      "Description": "Sacks slightly increase composure of other defensive linemen."
+    },
+    "Silver": {
+      "Description": "Sacks moderately increase composure of other defensive linemen."
+    },
+    "Gold": {
+      "Description": "Sacks significantly increase composure of other defensive linemen."
+    },
+    "Platinum": {
+      "Description": "Sacks give ultimate increase composure gain of other defensive linemen."
+    }
+  },
+  "Dot!": {
+    "Type": "Physical",
+    "Attribute": "",
+    "ShortDes": "",
+    "Archetypes": "",
+    "Bronze": {},
+    "Silver": {},
+    "Gold": {},
+    "Platinum": {}
+  },
+  "Double Dip": {
+    "Type": "Physical",
+    "Attribute": "",
+    "ShortDes": "",
+    "Archetypes": "",
+    "Bronze": {},
+    "Silver": {},
+    "Gold": {},
+    "Platinum": {}
+  },
+  "Downhill": {
+    "Type": "Physical",
+    "Attribute": "",
+    "ShortDes": "",
+    "Archetypes": "",
+    "Bronze": {},
+    "Silver": {},
+    "Gold": {},
+    "Platinum": {}
+  },
+  "Duress": {
+    "Type": "Physical",
+    "Attribute": "",
+    "ShortDes": "",
+    "Archetypes": "",
+    "Bronze": {},
+    "Silver": {},
+    "Gold": {},
+    "Platinum": {}
+  },
+  "Extender": {
+    "Type": "Physical",
+    "Attribute": "",
+    "ShortDes": "",
+    "Archetypes": "",
+    "Bronze": {},
+    "Silver": {},
+    "Gold": {},
+    "Platinum": {}
+  },
+  "Fan Favorite": {
+    "Type": "Mental",
+    "ShortDes": "",
+    "Positions": "",
+    "Bronze": {
+      "Description": "Slightly increased composure gains at home."
+    },
+    "Silver": {
+      "Description": "Moderately increased composure gains at home."
+    },
+    "Gold": {
+      "Description": "Significantly increased composure gains at home."
+    },
+    "Platinum": {
+      "Description": "Ultimate increased composure gains at home."
+    }
+  },
   
   "Press Pro": {
     "Type": "Physical"
