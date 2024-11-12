6228db5a6234713fa17490c209493b96c99b7634
Joshua McD <77360758+ExpensiveCoder@users.noreply.github.com>
2024-11-11 21:20:40 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,29 @@
-# Configuration file to store static information (stat categories, rating categories, position archetype, etc)
 
-# TODO: Arrays to hold the heading of the tracked stats (General, Passing, Rushing, Receiving, Defense, Blocking, Kicking)
-GEN_STATS = ['GP (Games Played)', 'DP (Downs Played)']
-PASSING_STATS = ['RATING', 'YARDS', 'TD', 'INT', 'LONG', 'SACKS', 'COMP', 'ATT', 'COMP%', 'YPA']
-RUSHING_STATS = []
-RECEIVING_STATS = []
-DEFENSE_STATS = ['SOLO', 'ASSISTS', 'TAK', 'TFL', 'SACK', 'INT', 'INT YDS', 'INT AVG', 'INT LG', 'DEFL', 'CTHA', 'FFUMB', 'TD']
-BLOCK_STATS = []
-KICK_STATS = []
+
+# Arrays to hold the heading of the tracked stats (General, Passing, Rushing, Receiving, Defense, Blocking, Kicking)
+PASSING_STATS = ['RATING', 'YARDS', 'TD', 'INT', 'LONG', 'SACKS', 'COMP', 'ATT', 'COMP%', 'YPA', 'GP', 'DP']
+RUSHING_STATS = ['CAR', 'YARDS', 'AVG', 'TD', 'AVG G', 'FUMB', 'BTK', 'LONG', '20+', 'GP', 'DP']
+RECEIVING_STATS = ['REC', 'YARDS', 'AVG', 'AVG G', 'TD', 'RAC', 'LONG', 'GP', 'DP']
+DEFENSE_STATS = ['SOLO', 'ASSISTS', 'TAK', 'TFL', 'SACK', 'INT', 'INT YDS', 'INT AVG', 'INT LG', 'DEFL', 'CTHA', 'FFUMB', 'TD', 'GP', 'DP']
+BLOCK_STATS = ['SACK', 'GP', 'DP']
+KICK_STATS = ['FGM', 'FGA', 'FG%', 'FG LNG', 'FG BLK', 'XPM', 'XPA', 'XP%', 'XPB', 'FGM29', 'FGA29', 'FGM39', 'FMA39', 'FGM39', 'FGA39', 'FGM49', 'FGA49', 'FGM50+', 'FGA50+', 'KO', 'TB', 'GP']
+PUNT_STATS = ['PUNTS', 'YARDS', 'AVG', 'NET YDS', 'NET AVG', 'BLOCK', 'IN20', 'TB', 'LONG', 'GP']
+KR_STATS = ['KR', 'YARDS', 'AVG', 'TD', 'LONG', 'GP']
+PR_STATS = ['PR', 'YARDS', 'AVG', 'TD', 'LONG', 'GP']
 
 # TODO: Arrays to hold the heading of the ratings tracked per position group (QB, )
 QB_RAT = ['OVR', 'SPD', 'ACC', 'AGI', 'COD', 'STR', 'AWR', 'THP', 'SAC', 'MAC', 'DAC', 'RUN', 'TUP',
           'CAR', 'BCV', 'BTK', 'TRK', 'SFA', 'SPM', 'JKM', 'BSK', 'PAC', 'IBL', 'STA', 'TGH', 'INJ']
-HB_RAT = []
+HB_RAT = ['OVR', 'SPD', 'ACC', 'AGI', 'COD', 'STR', 'AWR', 'CAR', 'BCV', 'BTK', 'TRK', 'SFA', 'SPM',
+          'JKM', 'CTH', 'CIT', 'SPC', 'SRR', 'MRR', 'DRR', 'RLS', 'PBK', 'PBP', 'PBF', 'RBK', 'RBP',
+          'RBF', 'LBK', 'IBL', 'RET', 'STA', 'TGH', 'INJ']
+WR_RAT = ['OVR', 'SPD', 'ACC', 'AGI', 'COD', 'STR', 'AWR', 'CTH', 'CIT', 'SPC', 'SRR', 'MRR', 'DRR',
+          'RLS', 'JMP', 'CAR', 'BCV', 'BTK', 'TRK', 'SFA', 'SPM', 'JKM', 'RBK', 'IBL', 'RET', 'STA',
+          'TGH', 'INJ']
+TE_RAT = ['OVR', 'SPD', 'ACC', 'AGI', 'COD', 'STR', 'AWR', 'CTH', 'CIT', 'SPC', 'SRR', 'MRR', 'DRR',
+          'RLS', 'PBK', 'PBP', 'PBF', 'RBK' ,'RBP', 'RBF', 'LBK', 'IBL', 'CAR', 'BTK', 'TRK', 'SFA',
+          'SPM', 'JKM', 'JMP', 'STA', 'TGH', 'INJ']
+
 DE_RAT = ['OVR', 'SPD', 'ACC', 'AGI', 'COD', 'STR', 'AWR', 'PRC', 'PMV', 'FMV', 'BSH',
           'TAK', 'POW', 'PUR', 'ZCV', 'MCV', 'CTH', 'JMP', 'IBL', 'STA', 'TGH', 'INJ']
 OLB_RAT = ['OVR', 'SPD', 'ACC', 'AGI', 'COD', 'STR', 'AWR', 'PRC', 'TAK', 'POW', 'BSH', 'PMV',
